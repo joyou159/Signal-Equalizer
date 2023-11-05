495785177d9ca30de8986afd89df544abae5219d
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2023-11-05 21:56:07 +0200

    start

---
+++ Signal.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Signal:
+    def __init__(self, name):
+        self.name = name
+        self.freq_components = []
+        self.data = []
+        self.time = []
+        self.maxFreq = None
+
+    def add_component(self, component):
+        self.components.append(component)
+
+    def change_sample_rate(self, new_sample_rate):
+        self.sample_rate = new_sample_rate
